018fef4f3aa78b361419b14ad68dc7ed5618fd35
SeungHyunBaek <back947@naver.com>
2020-11-02 16:11:33 +0900

    AndroidDeveloper-ConstraintLayout으로 반응형 UI 빌드(키프레임 애니메이션)

--- app/src/main/java/best/hyun/layout/MainActivity.kt
+++ app/src/main/java/best/hyun/layout/MainActivity.kt
@@ -2,11 +2,41 @@ package best.hyun.layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
 
+    var isShown: Boolean = false
+
+    var constraintSetShow = ConstraintSet()
+    var constraintSetHide = ConstraintSet()
+
+    lateinit var layout: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.keyframe_one)
+
+        layout = findViewById(R.id.layout)
+
+        constraintSetHide.clone(layout)
+        constraintSetShow.clone(this, R.layout.keyframe_two)
+    }
+
+    fun handleShowDetail(view: View) {
+        TransitionManager.beginDelayedTransition(layout)
+
+        if(isShown) {
+            // hide details
+            constraintSetHide.applyTo(layout)
+        } else {
+            // how details
+            constraintSetShow.applyTo(layout)
+        }
+
+        isShown = !isShown
     }
 }
