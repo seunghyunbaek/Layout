8faad74ff9fae19946f4a0c45f5a3a3095f0d632
SeungHyunBaek <back947@naver.com>
2020-11-03 10:39:24 +0900

    Add keyframeactivity

---
+++ app/src/main/java/best/hyun/layout/KeyFrameActivity.kt
@@ -0,0 +1,11 @@
+package best.hyun.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KeyFrameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
